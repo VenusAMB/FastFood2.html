2158f5783e0759f4fc21bc4f6649782c065b097a
VenusAMB <60484434+VenusAMB@users.noreply.github.com>
2020-03-20 12:46:02 -0400

    again

---
+++ FastFood2.js
@@ -0,0 +1,27 @@
+const myFunction = document.querySelector
+("myButton");
+
+menuicon.addEvenyListner("click", () => {
+    navbar.classList.toggle("change");
+});
+
+
+
+function myFunction() {
+    document.getElementById("myButton").classList.toggle("SUBMIT ORDER");
+}
+
+
+const myArray = ['Hamburger ', 'Cheeseburger ', 'Vegetarian Burger ', 'Vegan Burger ', 'Fish Burger ', 'French Fries ', 'Sewwt Potato Fries ', 'Onion Rings ', 'House Salad ', 'Sweet Tea ', 'Unsweet Tea ', 'Lemonade ', 'Sprite Soda ', 'Orange Soda ', 'Coke Soda'];
+console.log(myArray.toString());
+
+
+//var name = 'Hamburger';//
+
+//console.log (name);//
+
+//var i = "60";//
+
+//var hamburger = Number(i);//
+
+console.log (i-5)
